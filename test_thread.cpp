4a6974ce6ee7f7edb598a951fa44848d705bf75a
Daniel Wang <danielwpz@gmail.com>
2014-03-17 11:14:11 +0800

    Add multi-thread select() test case in test_thread.cpp

--- test_thread.cpp
+++ test_thread.cpp
@@ -1,3 +1,5 @@
+#include <unistd.h>
+#include <string.h>
 #include <iostream>
 #include <chrono>
 
@@ -6,15 +8,60 @@
 using namespace std;
 
 static int cnt = 0;
+static int std_in = 0;
 std::mutex cnt_mutex;	// lock for cnt
+std::mutex print_mutex;	// lock for print
 
-void fun(int &n)
+void uni_lock(int &n)
 {
 	std::unique_lock<std::mutex> ulock(cnt_mutex);
 
 	cout << "[" << cnt << ", " << n << "]";
 	cout << "In thread: " << std::this_thread::get_id() << endl;;
 	cnt++;
+}
+
+#define BUF_SIZE 6
+
+void multi_select()
+{
+	fd_set rd_fd;
+	struct timeval tv;
+	int ret;
+	char buf[BUF_SIZE];
+	std::thread::id thread_id = std::this_thread::get_id();
+
+
+	while(1) {
+		// reset fd_set
+		FD_ZERO(&rd_fd);
+		FD_SET(std_in, &rd_fd);
+		// reset timeval
+		tv.tv_sec = 5;
+		tv.tv_usec = 0;
+		// reset buffer
+		bzero(&buf, BUF_SIZE);
+
+		ret = select(std_in + 1, &rd_fd, NULL, NULL, &tv);
+
+		std::unique_lock<std::mutex> ulock(print_mutex);
+		cout << "[" << thread_id <<  ", ret=" << ret << "]" << flush;
+
+		if (ret == 0) {
+			cout << "select time out.\n";
+		}else if (ret < 0) {
+			cout << "select error.\n";
+		}else {
+			if (FD_ISSET(std_in, &rd_fd)) {
+				ssize_t rd_size;
+				rd_size = read(std_in, &buf, BUF_SIZE - 2);
+				buf[BUF_SIZE - 1] = '\0';
+
+				cout << "read() return " << rd_size << ", ";
+				printf("'%s'\n", buf);
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -41,8 +88,7 @@ int main(int argc, char **argv)
 	pool->start();
 
 	for (i = 0; i < run_times; i++) {
-		cout << "Next i = " << i << endl;
-		pool->run(std::bind(fun, i));
+		pool->run(multi_select);
 	}
 
 	// stop the pool when all task
